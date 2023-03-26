ad5f7b547992680536c12c8eb393f1c9be244261
clauds-macedo <yanalogic@gmail.com>
2023-03-26 20:31:12 -0300

    fix: corrige verificação na função hasMoreIds

--- syntactic/productions/declaration.cpp
+++ syntactic/productions/declaration.cpp
@@ -9,51 +9,40 @@
 #include "../utils/eat.cpp"
 #include "../utils/verify-productions.cpp"
 
-regex types("integer|pilha_of_real|pilha_of_integer|real");
+regex types ("integer|pilha_of_real|pilha_of_integer|real");
 
-bool checkVarDeclaration(vector<Token> tokens, int *currentToken)
-{
-    if (verify_content(tokens, currentToken, "var"))
-    {
+bool checkVarDeclaration(vector<Token> tokens, int* currentToken) {
+    if (verify_content(tokens, currentToken, "var")) {
         return true;
     }
     // caso  não seja "var", volta 1 posição no vector de tokens
-    else
-        *currentToken -= 1;
+    else *currentToken -= 1;
     return false;
 }
 
-bool hasIdAfterVar(vector<Token> tokens, int *currentToken)
-{
-    if (checkVarDeclaration(tokens, currentToken))
-    {
+bool hasIdAfterVar(vector<Token> tokens, int* currentToken) {
+    if (checkVarDeclaration(tokens, currentToken)) {
         return identifier(tokens, currentToken);
     }
     return false;
 }
 
-bool hasTypeAfterId(vector<Token> tokens, int *currentToken)
-{
-    if (hasIdAfterVar(tokens, currentToken))
-    {
+bool hasTypeAfterId(vector<Token> tokens, int* currentToken) {
+    if (hasIdAfterVar(tokens, currentToken)) {
         // verifica se tem ":" para analisar a tipagem
-        if (verify_content(tokens, currentToken, ":"))
-        {
+        if (verify_content(tokens, currentToken, ":")) {
             // verifica se a tipagem é valida
-            if (regex_match(tokens[*currentToken].content, types))
-            {
+            if (regex_match(tokens[*currentToken].content, types)) {
                 return eat(tokens, currentToken);
             }
-            else
-            {
+            else {
                 *currentToken -= 1;
                 string error = "Esperava-se uma tipagem (integer, pilha_of_real, pilha_of_integer, real) após a declaração da variável ";
-                string varError = error.append(tokens[*currentToken - 1].content);
+                string varError = error.append(tokens[*currentToken-1].content);
                 throw std::invalid_argument(varError);
             }
         }
-        else
-        {
+        else {
             *currentToken -= 1;
             string error = "Esperava o caractere : após a variável ";
             string varError = error.append(tokens[*currentToken].content);
@@ -63,23 +52,20 @@ bool hasTypeAfterId(vector<Token> tokens, int *currentToken)
     return false;
 }
 
-bool hasMoreIds(vector<Token> tokens, int *currentToken)
-{
-    if (hasTypeAfterId(tokens, currentToken))
-    {
-        while (true)
-        {
+bool hasMoreIds(vector<Token> tokens, int* currentToken) {
+    if (!verify_content(tokens, currentToken, ";")) {
+        while (true) {
+            cout << tokens[*currentToken].content << "\n";
             eat(tokens, currentToken);
-            if (verify_content(tokens, currentToken, ";"))
-            {
+            if (verify_content(tokens, currentToken, ";")) {
                 return true;
             }
         }
     }
+    else return eat(tokens, currentToken);
     return false;
 }
 
-bool declaration(vector<Token> tokens, int *currentToken)
-{
+bool declaration(vector<Token> tokens, int* currentToken) {
     return verify_productions(tokens, currentToken, {hasMoreIds});
 }
